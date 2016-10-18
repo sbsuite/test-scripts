ca43bf0ef7128c626ba7feef463d4b978fe87441
Robert <rob@robertmcintosh.ca>
2016-10-18 13:38:44 -0400

    adding and removing

--- pre-setup.ps1
+++
@@ -1,5 +0,0 @@
-$fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER\packages -Filter *.wxs -Recurse | %{$_.FullName} 
-foreach($fragment in $fragments)
-{
-    Copy-Item $fragment  $env:APPVEYOR_BUILD_FOLDER\setup\$env:SETUP_PROJECT_NAME\fragments
-}
