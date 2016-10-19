191b4817c42821f3727b964a93f33eb0444b487a
Robert <rob@robertmcintosh.ca>
2016-10-19 09:15:50 -0400

    error in using filter

--- report-coverage.ps1
+++ report-coverage.ps1
@@ -17,6 +17,6 @@ $coveralls
 $targetArgs
 
 
-& $openCover -register:user -target:nunit3-console.exe -targetargs:$targetArgs -filter:$testFilter -output:OpenCover.xml
+& $openCover -register:user -target:nunit3-console.exe -targetargs:$targetArgs -output:OpenCover.xml
 $env:APPVEYOR_BUILD_NUMBER
 & $coveralls --opencover -i OpenCover.xml --repoToken $env:COVERALLS_REPO_TOKEN --useRelativePaths --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_NUMBER --serviceName appveyor
