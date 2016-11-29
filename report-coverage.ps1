a77fa22078cda7ec93a2881d330b31832abf54d3
rwmcintosh <rob@robertmcintosh.ca>
2016-11-29 08:54:01 -0500

    return code

--- report-coverage.ps1
+++ report-coverage.ps1
@@ -21,7 +21,7 @@ Write-Host "nUnit args $($targetArgs)" -ForegroundColor Green
 
 Write-Host "$($openCover) -register:user -target:nunit3-console.exe -targetargs:$($targetArgs) -filter:$($reportFilter) -output:OpenCover.xml" -ForegroundColor Green
 
-& $openCover -register:user -target:nunit3-console.exe -targetargs:$targetArgs -filter:$reportFilter -output:OpenCover.xml
+& $openCover -register:user -target:nunit3-console.exe -targetargs:$targetArgs -filter:$reportFilter -output:OpenCover.xml -returntargetcode
 $testCode = $lastexitcode
 $env:APPVEYOR_BUILD_NUMBER
 & $coveralls --opencover -i OpenCover.xml --repoToken $env:COVERALLS_REPO_TOKEN --useRelativePaths --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_NUMBER --serviceName appveyor
