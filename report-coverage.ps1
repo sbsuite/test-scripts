8150bd5af4013dfb031da0a6e6d10a5e8bff6593
rwmcintosh <rob@robertmcintosh.ca>
2016-11-29 07:56:06 -0500

    adding return code to open cover executable

--- report-coverage.ps1
+++ report-coverage.ps1
@@ -22,5 +22,7 @@ Write-Host "nUnit args $($targetArgs)" -ForegroundColor Green
 Write-Host "$($openCover) -register:user -target:nunit3-console.exe -targetargs:$($targetArgs) -filter:$($reportFilter) -output:OpenCover.xml" -ForegroundColor Green
 
 & $openCover -register:user -target:nunit3-console.exe -targetargs:$targetArgs -filter:$reportFilter -output:OpenCover.xml
+$testCode = $lastexitcode
 $env:APPVEYOR_BUILD_NUMBER
 & $coveralls --opencover -i OpenCover.xml --repoToken $env:COVERALLS_REPO_TOKEN --useRelativePaths --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_NUMBER --serviceName appveyor
+exit $testCode
